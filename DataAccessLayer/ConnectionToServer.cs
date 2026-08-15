11bea772991007afdf24c8cb33cd7b5e8d9a8543
Kareem_Gabayen <gh.kareeem2004@gmail.com>
2026-08-15 10:58:28 +0300

    Initial commit for DVLDProject

---
+++ DataAccessLayer/ConnectionToServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static  class Connection
+    {
+        public static string ConnectionString = "Server=.;Database=ContactsDB;User Id=sa;Password=123456;";
+    }
+}
